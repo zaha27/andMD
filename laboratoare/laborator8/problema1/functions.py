c747caf7c9499e225e6c909ba817aaa9bf6c941f
zaha27 <andrei.zaharia2005@gmail.com>
2025-04-09 12:27:01 +0300

    problema 1

---
+++ laboratoare/laborator8/problema1/functions.py
@@ -0,0 +1,3 @@
+def g(x, y, z) :
+    return x ** 2 + 3 * y + 5 * z
+
---
+++ laboratoare/laborator8/problema1/main.py
@@ -0,0 +1,17 @@
+import functions as funct
+
+def main ():
+
+    A_one_1 = range(-10, 10, 1)
+    A_one_2 = range(-10, 10, 1)
+    A_one_3 = range(-10, 10, 1)
+    A_two = range(-10, 10, 1)
+
+    B_one = list(map(funct.g, A_one_1, A_one_2, A_one_3))
+    B_two = list(map(lambda x: 2*x, A_two))
+
+    print(B_one)
+    print(B_two)
+
+if __name__ == "__main__":
+    main()
