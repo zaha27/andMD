8319646919b86abc6b5f64aebd06589347454f5b
zaha27 <andrei.zaharia2005@gmail.com>
2025-05-08 15:20:34 +0300

    final p6

--- laboratoare/laborator3/p6/main.py
+++ laboratoare/laborator3/p6/main.py
@@ -5,16 +5,16 @@ def main():
     print("Este tautologie?" if f.e_tautologie_two(f.expresie1) else "NU este tautologie.")
 
     print("\nPunctul 2:")
-    print("Este tautologie?" if f.e_tautologie_two(f.expresie2) else "NU este tautologie.")
+    print("Este tautologie" if f.e_tautologie_two(f.expresie2) else "NU este tautologie.")
 
     print("\nPunctul 3:")
-    print("Este tautologie?" if f.e_tautologie_three(f.expresie3) else "NU este tautologie.")
+    print("Este tautologie" if f.e_tautologie_three(f.expresie3) else "NU este tautologie.")
 
     print("\nPunctul 4:")
-    print("Este tautologie?" if f.e_tautologie_three(f.expresie4) else "NU este tautologie.")
+    print("Este tautologie" if f.e_tautologie_three(f.expresie4) else "NU este tautologie.")
 
     print("\nPunctul 5:")
-    print("Este tautologie?" if f.e_tautologie_two(f.expresie5) else "NU este tautologie.")
+    print("Este tautologie" if f.e_tautologie_two(f.expresie5) else "NU este tautologie.")
 
 if __name__ == "__main__":
     main()
---
+++ laboratoare/laborator3/p6/oplogic.py
@@ -0,0 +1,18 @@
+def implicatie(p, q):
+    if p:
+        return q
+    else:
+        return True
+
+def implica(p, q):
+    return not p or q
+
+def biconditie(p, q):
+    implicatie1 = implicatie(p, q)
+    implicatie2 = implicatie(q, p)
+    rezultat = implicatie1 and implicatie2
+    return rezultat
+
+def sau_exclusiv(p, q):
+    rezultat = (p and not(q)) or (not(p) and q)
+    return rezultat
