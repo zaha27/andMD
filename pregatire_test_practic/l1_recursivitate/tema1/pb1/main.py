cb5fe4c379674d2b812b7317fb8b97eb6fec94d9
zaha27 <andrei.zaharia2005@gmail.com>
2025-05-19 16:19:51 +0300

    l1 pb1

---
+++ pregatire_test_practic/l1_recursivitate/tema1/pb1/main.py
@@ -0,0 +1,10 @@
+import functions as f
+
+def main() :
+    n = 7
+    rez = f.factorial(n)
+    print(f"rezultatul factorialului {n} este {rez}")
+    return
+
+if __name__ == '__main__' :
+    main()
