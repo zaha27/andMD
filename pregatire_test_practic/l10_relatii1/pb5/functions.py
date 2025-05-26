8cd7ec62c67b08b2fb8441e2c7d577005771db15
zaha27 <andrei.zaharia2005@gmail.com>
2025-05-26 22:18:59 +0300

    l10

--- pregatire_test_practic/l10_relatii1/pb5/functions.py
+++ pregatire_test_practic/l10_relatii1/pb5/functions.py
@@ -13,3 +13,39 @@ def citire_matrice(n, m) :
 def afisare_matrice(n, A) : 
     for i in range(n) :
         print(A[i])
+
+def init_matrice(n: int ) -> list :
+    A = []
+    for i in range(n) :
+        lista = []
+        for j in range(n) :
+            lista.append(0)
+        A.append(lista)
+    return A
+
+def inversare_matrice(n, A) :
+    A_inv = init_matrice(n)
+    for i in range(n) :
+        for j in range(n) :
+            A_inv[i][j] = A[j][i]
+    return A_inv
+
+def complement_matrice(n, A) :
+    A_compl = init_matrice(n)
+    for i in range(n) :
+        for j in range(n) :
+            if A[i][j] == 0 : 
+                A_compl[i][j] = 1
+            else : A_compl[i][j] = 0    
+    return A_compl
+
+def ridicare_la_patrat(n, A) :
+    A_patrat = init_matrice(n)
+
+    for i in range(n) :
+        for j in range(n) :
+            sum = 0
+            for k in range(n) :
+                sum = sum or (A[i][k] and A[k][j])
+            A_patrat[i][j] = sum
+    return A_patrat
--- pregatire_test_practic/l10_relatii1/pb5/main.py
+++ pregatire_test_practic/l10_relatii1/pb5/main.py
@@ -1,8 +1,16 @@
 import functions as f
 
 def main() :
-    A = f.citire_matrice(3, 3)
-    f.afisare_matrice(3, A)
+    n = int(input("n = "))
+    A = f.citire_matrice(n, n)
+    f.afisare_matrice(n, A)
+    A_inv = f.inversare_matrice(n, A)
+    f.afisare_matrice(n, A_inv)
+    A_compl = f.complement_matrice(n, A)
+    f.afisare_matrice(n, A_compl)
+    A_patrat = f.ridicare_la_patrat(n, A)
+    f.afisare_matrice(n, A_patrat)
+
     
 
 if __name__ == "__main__" :
